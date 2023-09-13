45c8fcd99ebbe4dd3e3fdedc17a9f9eda019a149
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-09-13 13:55:19 +0300

    1. Слоеная архитектура. [#504851]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -4,20 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.repository.CandidatesRepository;
-import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
-
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
+import ru.job4j.dreamjob.service.CandidateService;
+import ru.job4j.dreamjob.service.SimpleCandidateService;
 
 @Controller
 @RequestMapping("/candidates")
 public class CandidateController {
-    private final CandidatesRepository candidatesRepository = MemoryCandidateRepository.getInstance();
+    private final CandidateService candidatesService = SimpleCandidateService.getInstance();
 
     @GetMapping
     public String getAll(Model model) {
-        model.addAttribute("candidates", candidatesRepository.findAll());
+        model.addAttribute("candidates", candidatesService.findAll());
         return "candidates/list";
     }
 
@@ -28,13 +25,13 @@ public class CandidateController {
 
     @PostMapping("/create")
     public String create(@ModelAttribute Candidate candidate) {
-        candidatesRepository.save(candidate);
+        candidatesService.save(candidate);
         return "redirect:/candidates";
     }
 
     @GetMapping("/{id}")
     public String getById(Model model, @PathVariable int id) {
-        var candidateOptional = candidatesRepository.findById(id);
+        var candidateOptional = candidatesService.findById(id);
         if (candidateOptional.isEmpty()) {
             model.addAttribute("message", "Резюме с указанным идентификатором не найдена");
             return "errors/404";
@@ -45,7 +42,7 @@ public class CandidateController {
 
     @PostMapping("/update")
     public String update(@ModelAttribute Candidate candidate, Model model) {
-        var isUpdated = candidatesRepository.update(candidate);
+        var isUpdated = candidatesService.update(candidate);
         if (!isUpdated) {
             model.addAttribute("message", "Резюме с указанным идентификатором не найдена");
             return "errors/404";
@@ -55,7 +52,7 @@ public class CandidateController {
 
     @GetMapping("/delete/{id}")
     public String delete(Model model, @PathVariable int id) {
-        var isDeleted = candidatesRepository.deleteById(id);
+        var isDeleted = candidatesService.deleteById(id);
         if (!isDeleted) {
             model.addAttribute("message", "Резюме с указанным идентификатором не найдена");
             return "errors/404";
